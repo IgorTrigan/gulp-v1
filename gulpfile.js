d47f442fd4f05c6be8c2c9bef67c5f0221d2a0e8
Artemchuk Ihor <250314march@gmail.com>
2022-03-09 00:30:59 +0200

    added plugin gulp-size

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ const { src, dest } = require('gulp')
 
 const fileInclude = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
+const size = require('gulp-size')
 
 const html = () => {
   return src('./src/html/*.html')
     .pipe(fileInclude())
+    .pipe(size({ title: 'Before data compression' }))
     .pipe(
       htmlmin({
         collapseWhitespace: true,
       })
     )
+    .pipe(size({ title: 'After data compression' }))
     .pipe(dest('./public'))
 }
 
