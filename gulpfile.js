5800734b9a49ac60ff82b6b9fe959a6bb10f3903
Artemchuk Ihor <250314march@gmail.com>
2022-03-09 00:23:55 +0200

    add plugin gulp-htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const { src, dest } = require('gulp')
 
 const fileInclude = require('gulp-file-include')
+const htmlmin = require('gulp-htmlmin')
 
 const html = () => {
-  return src('./src/html/*.html').pipe(fileInclude()).pipe(dest('./public'))
+  return src('./src/html/*.html')
+    .pipe(fileInclude())
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest('./public'))
 }
 
 exports.html = html
