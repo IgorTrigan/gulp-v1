e14bee7b3dee861dc326342e997216c3c34997fc
Artemchuk Ihor <250314march@gmail.com>
2022-03-09 11:53:15 +0200

    gulp-plumber+notify

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,18 @@ const fileInclude = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
 const size = require('gulp-size')
 const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
 
 const html = () => {
   return src('./src/html/*.html')
-    .pipe(plumber())
+    .pipe(
+      plumber({
+        errorHandler: notify.onError((error) => ({
+          title: 'HTML',
+          message: error.message,
+        })),
+      })
+    )
     .pipe(fileInclude())
     .pipe(size({ title: 'Before data compression' }))
     .pipe(
