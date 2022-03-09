6b85dda286d3e57cfed57ce0fb1b592cd29edb7a
Artemchuk Ihor <250314march@gmail.com>
2022-03-09 11:34:35 +0200

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const { src, dest, watch, series, parallel } = require('gulp')
+const browserSync = require('browser-sync').create()
 
 const fileInclude = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
@@ -15,10 +16,20 @@ const html = () => {
     )
     .pipe(size({ title: 'After data compression' }))
     .pipe(dest('./public'))
+    .pipe(browserSync.stream())
 }
 //watchers
 const watcher = () => {
   watch('./src/html/**/*.html', html)
+}
+
+//server
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: './public',
+    },
+  })
 }
 
 // Tasks
@@ -27,4 +38,4 @@ exports.html = html
 exports.watch = watcher
 
 //assembly
-exports.dev = series(html, watcher)
+exports.dev = series(html, parallel(watcher, server))
